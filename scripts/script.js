3c4481bfed198a5fb4b33e9e564653e95b46ae01
Mindaugas Jukna <mindaugas9494@hotmail.com>
2018-09-30 14:13:33 +0200

    keg level chart done

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,17 @@ const servingP = document.querySelector("#top-box .serving p");
 const servingNumber = document.querySelector("#top-box .serving p");
 const perfNumber = document.querySelector("#top-box .performance p");
 const perfChartDOM = document.querySelector("#perf-chart").getContext("2d");
+const kegLevelChartDOM = document
+  .querySelector("#keg-level-chart")
+  .getContext("2d");
 
 const updateInterval = 1000; //update data every 1s;
 const updatePerformanceInterval = 15000; //update performance every 15s
+const updateKegLevelInterval = 1000; //update keg level every 1s
 
-let data, update, updatePerformance, perfChart;
+let data, update, updatePerformance, perfChart, kegLevelChart, updateKegLevel;
+let kegLevel = [];
+
 //perf is used to calculate performance
 let perf = {
   avgT: 10000,
@@ -54,8 +60,15 @@ function init() {
     displayPerf();
   }, updatePerformanceInterval);
 
+  updateKegLevel = setInterval(() => {
+    updateKegLevelData();
+  }, updateKegLevelInterval);
+
   displayData();
   displayPerfChart();
+  displayKegLevelChart();
+  updateKegLevelData();
+  updateKegLevelChart();
 }
 
 /* ==========================================================================
@@ -136,17 +149,15 @@ function calcPerf() {
     });
   }
 
-  /*
-  Use these console logs to understand this function
+  //Use these console logs to understand this function
 
-  console.log("Current:" + JSON.stringify(perf.currentS));
+  /*  console.log("Current:" + JSON.stringify(perf.currentS));
   console.log("Previous:" + JSON.stringify(perf.previousS));
   console.log("Difference 1: " + JSON.stringify(newClient));
   console.log("Difference 2: " + JSON.stringify(servedClient));
   console.log("AVG Time: " + JSON.stringify(perf.timePerClient));
-  console.log("======");
+  console.log("======"); */
 
-  */
   //Add this update rounds clients to be used for next round
   perf.previousS = perf.currentS.slice();
 
@@ -173,7 +184,7 @@ function displayPerf() {
 
   //Calculcate percentage in relation to very fast serving of x seconds.
   perf.avgPerf = Math.floor((perf.avgT / averageTime) * 100);
-  console.log(perf.avgPerf);
+  //console.log(perf.avgPerf);
   if (isNaN(perf.avgPerf)) {
     //no performance data available display N/A
     perfNumber.textContent = "N/A";
@@ -231,4 +242,114 @@ function updatePerfChart() {
     perfChart.data.datasets[0].backgroundColor = [color];
     perfChart.update();
   }
+}
+
+function displayKegLevelChart() {
+  kegLevelChart = new Chart(kegLevelChartDOM, {
+    type: "bar",
+    // The data for our dataset
+    data: {
+      labels: [
+        "ELH",
+        "FTA",
+        "HBL",
+        "GTH",
+        "HEA",
+        "MIT",
+        "R26",
+        "RCH",
+        "SLR",
+        "SMP"
+      ],
+      datasets: [
+        {
+          label: "Keg level in cl",
+          backgroundColor: ["#39B54A"],
+
+          data: [0, 10, 5, 2, 20, 30, 45]
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              suggestedMin: 0,
+              suggestedMax: 2500,
+              beginAtZero: true,
+              min: 0,
+              max: 2500,
+              stepSize: 500
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+
+function updateKegLevelData() {
+  kegLevel = [];
+  data.taps.forEach(keg => {
+    let beerVar = keg.beer.toLowerCase().replace(/ /g, "");
+    //console.log(beerVar);
+    let short = {
+      elhefe: "ELH",
+      fairytaleale: "FTA",
+      hollabacklager: "HBL",
+      githop: "GTH",
+      hoppilyeverafter: "HEA",
+      mowintime: "MIT",
+      row26: "R26",
+      ruinedchildhood: "RCH",
+      sleighride: "SLR",
+      steampunk: "SMP"
+    }[beerVar];
+    //console.log(short);
+
+    let beer = {
+      name: keg.beer,
+      short: short,
+      level: keg.level
+    };
+
+    kegLevel.push(beer);
+  });
+
+  console.log(kegLevel);
+  updateKegLevelChart();
+}
+
+function updateKegLevelChart() {
+  kegLevelChart.data.labels = [];
+  kegLevelChart.data.datasets[0].data = [];
+  kegLevelChart.data.datasets[0].backgroundColor = [];
+
+  let status;
+
+  kegLevel.forEach(keg => {
+    if (keg.level > 1500) {
+      status = "good";
+    } else if (keg.level < 500) {
+      status = "bad";
+    } else if (keg.level > 500 && keg.level < 1500) {
+      status = "okay";
+    }
+
+    const color = {
+      bad: "#ED1C24",
+      okay: "#F7931E",
+      good: "#39B54A"
+    }[status];
+
+    kegLevelChart.data.labels.push(keg.short);
+    kegLevelChart.data.datasets[0].data.push(keg.level);
+    kegLevelChart.data.datasets[0].backgroundColor.push(color);
+  });
+
+  kegLevelChart.update();
 }
